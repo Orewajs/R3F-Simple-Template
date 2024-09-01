426ceee1b57aa47dece71b2a3d37a3e6b94f26d5
Abdou JR <abdou-elboursoumi@outlook.fr>
2024-09-01 07:13:44 +0200

    first commit

---
+++ src/experience/Index.jsx
@@ -0,0 +1,12 @@
+const Index = () => {
+  return (
+    <>
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshBasicMaterial color={"red"} />
+      </mesh>
+    </>
+  );
+};
+
+export default Index;
